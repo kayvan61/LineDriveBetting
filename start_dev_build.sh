c6f3d4adf43ae395db4d3a38f36dab63f65a986b
kayvan61 <kayvan.mansoor@gmail.com>
2019-10-08 17:44:26 -0500

    Script: auto inits react app if first time running

--- start_dev_build.sh
+++ start_dev_build.sh
@@ -1,2 +1,7 @@
-cd website/line-drive-betting/
+_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/"
+_NODE_DIR="${_DIR}website/line-drive-betting/"
+cd ${_NODE_DIR}
+if [ ! -d "${_NODE_DIR}/node_modules" ]; then
+  npm install
+fi
 npm start
