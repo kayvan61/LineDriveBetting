f120e77c860d799c50667e86f16ea87d7a216100
kayvan61 <kayvan.mansoor@gmail.com>
2019-10-10 17:44:07 -0500

    UI: added the empty js file for homepage to keep repo neat. changed search to log-in to match future function better.

--- website/line-drive-betting/src/GlobalNavBar.js
+++ website/line-drive-betting/src/GlobalNavBar.js
@@ -16,9 +16,8 @@ class GlobalNavbar extends React.Component {
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
+    <Form inline>   
+      <Button variant="outline-info">Log-in</Button>
     </Form>
   </Navbar>
 </>
---
+++ website/line-drive-betting/src/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalNavbar from './GlobalNavBar'
+
+class Homepage extends React.Component {
+    render() {
+	return (
+	    <GlobalNavbar />
+	)
+  }
+}
+
+export default Homepage;
