0d456df758fec7798535ca596a6754ce05b5dd9d
erickli42 <erickli42@utexas.edu>
2019-10-11 14:00:39 -0500

    Added correct links in navbar

--- website/line-drive-betting/src/LoginPage.js
+++ website/line-drive-betting/src/LoginPage.js
@@ -1,11 +1,9 @@
 import React from "react";
-import GlobalNavbar from "./components/GlobalNavBar";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <div>
-        <GlobalNavbar />
         <h1>LOGIN PAGE</h1>
       </div>
     );
