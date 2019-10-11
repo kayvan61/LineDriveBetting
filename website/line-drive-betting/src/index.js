6055293225d4c0f272c0c3f1121c20b59cb9072a
kayvan61 <kayvan.mansoor@gmail.com>
2019-10-11 16:58:53 -0500

    added style sheet with background and text color removed example from HomePage.js

--- website/line-drive-betting/src/index.js
+++ website/line-drive-betting/src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './static/css/maincss.css'
 
-document.body.style = "background: #333333;";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
