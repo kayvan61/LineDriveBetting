33d6c5d22cd19e8ae73fd901d38c4b6c955c4fc5
Frank <43185092+Frankutexas@users.noreply.github.com>
2019-10-11 17:01:54 -0500

    modified basic bet now page

--- website/line-drive-betting/src/BetNowPage.js
+++ website/line-drive-betting/src/BetNowPage.js
@@ -1,12 +1,47 @@
 import React from "react";
 import GlobalNavbar from "./components/GlobalNavBar";
-
+import { slide as Menu } from "react-burger-menu";
 class BetNowPage extends React.Component {
   render() {
     return (
       <div>
         <GlobalNavbar />
-        <h1>BET NOW PAGE</h1>
+        <body style={{}}>
+          <h1 style={{ color: "pink" }}>BET NOW PAGE</h1>
+          <h2>Betting Links</h2>
+          <ul>
+            <li>
+              <a href="https://betnow.eu"> https://betnow.eu </a>
+            </li>
+            <li>
+              <a href="https://www.mybookie.ag/sportsbook/">
+                {" "}
+                https://www.mybookie.ag/sportsbook/
+              </a>
+            </li>
+            <li>
+              <a href="https://www.sportsbetting.ag/">
+                {" "}
+                https://www.sportsbetting.ag
+              </a>{" "}
+            </li>
+            <li>
+              <a href="https://xbet.ag/">https://xbet.ag/</a>
+            </li>
+            <li>
+              <a href="https://www.betonline.ag/">https://www.betonline.ag/</a>
+            </li>
+          </ul>
+          <h2> To learn more about NFL line-betting follow </h2>
+          <ul>
+            <li>
+              <a href="https://www.gamblingsites.org/sports-betting/sites/nfl/">
+                {" "}
+                GAMBLINGSITES.org
+              </a>
+            </li>
+          </ul>
+        </body>
       </div>
     );
   }
