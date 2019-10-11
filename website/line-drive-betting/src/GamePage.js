c88daff41b2c84c97c432f0a8dbe387e53af8c19
Miguel <Garzarobm@utexas.edu>
2019-10-11 16:10:24 -0500

    added table to game page

--- website/line-drive-betting/src/GamePage.js
+++ website/line-drive-betting/src/GamePage.js
@@ -1,5 +1,10 @@
 import React from "react";
 import GlobalNavbar from "./components/GlobalNavBar";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table"
+
+
+
 
 class GamePage extends React.Component {
   render() {
@@ -7,9 +12,46 @@ class GamePage extends React.Component {
       <div>
         <GlobalNavbar />
         <h1>GAME PAGE</h1>
-      </div>
+        
+        <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+
+
+            </div>
+            
+
+
     );
+    
   }
+  
 }
 
 export default GamePage;
