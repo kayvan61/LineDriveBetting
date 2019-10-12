ca62aeafbef43d82f1c9af4b435a157086ecf7d9
erickli42 <erickli42@utexas.edu>
2019-10-12 16:27:40 -0500

    Bet Now page styling change

--- website/line-drive-betting/src/BetNowPage.js
+++ website/line-drive-betting/src/BetNowPage.js
@@ -1,19 +1,26 @@
 import React from "react";
 import GlobalNavbar from "./components/GlobalNavBar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import logo1 from "./static/images/betnow.png";
 import logo2 from "./static/images/mybookie.png";
 import logo3 from "./static/images/sportsbets.png";
 import logo4 from "./static/images/xbets.png";
 import logo5 from "./static/images/onlinebet.png";
+import logo6 from "./static/images/gamblingsitesorg.png";
+
 class BetNowPage extends React.Component {
   render() {
     return (
       <div>
         <GlobalNavbar />
+        <div style={{ textAlign: "center", marginBottom: 70, marginTop: 20 }}>
+          <h2>Betting Links</h2>
+        </div>
 
-        {/* <h1 style={{ color: "pink" }}>BET NOW PAGE</h1> */}
-        <h2>Betting Links</h2>
-        <ul>
+        {/* <ul>
           <li>
             <a href="https://betnow.eu"> https://betnow.eu </a>
           </li>
@@ -44,33 +51,73 @@ class BetNowPage extends React.Component {
               GAMBLINGSITES.org
             </a>
           </li>
-        </ul>
-        <br></br>
-        <a href="https://betnow.eu">
-          {" "}
-          <img src={logo1} alt="BetNow logo" width="100" height="50"></img>
-        </a>
-        <a href="https://www.mybookie.ag/sportsbook/">
-          {" "}
-          <img src={logo2} alt="BetNow logo" width="100" height="50"></img>
-        </a>
-        <a href="https://www.sportsbetting.ag/">
-          {" "}
-          <img src={logo3} alt="BetNow logo" width="100" height="50"></img>
-        </a>
-        <a href="https://xbet.ag/">
-          {" "}
-          <img src={logo4} alt="BetNow logo" width="100" height="50"></img>
-        </a>
-        <a href="https://www.betonline.ag/">
-          {" "}
-          <img
-            src={logo5}
-            alt="Online Betting logo"
-            width="100"
-            height="50"
-          ></img>
-        </a>
+        </ul> */}
+        <Container>
+          <Row style={{ marginBottom: 50 }}>
+            <Col>
+              <a href="https://betnow.eu">
+                <img
+                  src={logo1}
+                  alt="BetNow logo"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://www.mybookie.ag/sportsbook/">
+                <img
+                  src={logo2}
+                  alt="BetNow logo"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://www.sportsbetting.ag/">
+                <img
+                  src={logo3}
+                  alt="BetNow logo"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <a href="https://xbet.ag/">
+                <img
+                  src={logo4}
+                  alt="BetNow logo"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://www.betonline.ag/">
+                <img
+                  src={logo5}
+                  alt="Online Betting logo"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+            <Col>
+              <a href="https://www.betonline.ag/">
+                <img
+                  src={logo6}
+                  alt="Gambling sites org"
+                  width="100%"
+                  height="100%"
+                ></img>
+              </a>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
