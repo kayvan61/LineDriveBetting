6934d36ccc095d2b90d79e173e963e58593e4642
erickli42 <erickli42@utexas.edu>
2019-10-25 21:14:26 -0500

    fixed warning about unused Link

--- website/line-drive-betting/src/LoginPage.js
+++ website/line-drive-betting/src/LoginPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter, Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { instanceOf } from "prop-types";
 import { withCookies, Cookies } from "react-cookie";
